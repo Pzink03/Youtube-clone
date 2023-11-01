501267e807b0cdb0b41106bb211afe6834af89f4
Pete <pete.zink03@gmail.com>
2023-11-01 19:21:05 -0400

    Updated Video Section

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,11 +2,16 @@ import {VariantProps,cva} from "class-variance-authority"
 import { ComponentProps } from "react"
 import { twMerge } from "tailwind-merge"
 
-const buttonStyles = cva(["hover:bg-secondary-hover", "transition-colors"], {
+export const buttonStyles = cva(["hover:bg-secondary-hover", "transition-colors"], {
     variants:{
         variant: {
             default: ["bg-secondary", "hover:bg-secondary-hover"],
             ghost: ["hover:bg-gray-100"],
+            dark: [
+                "bg-secondary-dark",
+                "hover:bg-secondary-dark-hover",
+                'text-secondary',
+            ]
         },
         size: {
             default: [" rounded", "p-2"],
